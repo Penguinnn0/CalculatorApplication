c2711a5e0fb56e68053179c43b1d90bd82d1a1b3
Jericho <Jericho@JEKJEK>
2023-09-18 13:13:03 +0800

    Add project files.

---
+++ CalculatorApplication/FrmCalculator.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CalculatorApplication
+{
+    public partial class FrmCalculator : Form
+    {
+        CalculatorClass cal = new CalculatorClass();
+        double num1, num2;
+
+        public FrmCalculator()
+        {
+            InitializeComponent();         
+        }
+
+        private void FrmCalculator_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnEqual_Click(object sender, EventArgs e)
+        {
+            num1 = Convert.ToDouble(txtBoxInput1.Text);
+            num2 = Convert.ToDouble(txtBoxInput2.Text);
+
+            if (cbOperator.SelectedIndex == 0)
+            {
+                cal.CalculateEvent += new Formula<double>(cal.GetSum);
+                lblDisplayTotal.Text = cal.GetSum(num1, num2).ToString();
+                cal.CalculateEvent -= new Formula<double>(cal.GetSum);
+            }
+            else if (cbOperator.SelectedIndex == 1)
+            {
+                cal.CalculateEvent += new Formula<double>(cal.GetDifference);
+                lblDisplayTotal.Text = cal.GetDifference(num1, num2).ToString();
+                cal.CalculateEvent -= new Formula<double>(cal.GetDifference);
+            }
+            else if (cbOperator.SelectedIndex == 2)
+            {
+                cal.CalculateEvent += new Formula<double>(cal.GetProduct);
+                lblDisplayTotal.Text = cal.GetProduct(num1, num2).ToString();
+                cal.CalculateEvent -= new Formula<double>(cal.GetProduct);
+            }
+            else if (cbOperator.SelectedIndex == 3)
+            {
+                cal.CalculateEvent += new Formula<double>(cal.GetQuotient);
+                lblDisplayTotal.Text = cal.GetQuotient(num1, num2).ToString();
+                cal.CalculateEvent -= new Formula<double>(cal.GetQuotient);
+            }
+
+        }
+    }
+}
